4e9a35e431985d6df561a7961030b205fae2d263
Emiliano Alvarez <MetalEmi7@gmail.com>
2017-11-23 03:23:29 -0300

    Initial commit

---
+++ apirest/classes/personaApi.php
@@ -0,0 +1,254 @@
+<?php
+require_once 'persona.php';
+require_once 'IGenericDAO.php';
+
+use Slim\Http\UploadedFile;
+
+class personaApi extends Persona implements IGenericDAO
+{
+    
+    function getById($request, $response, $args)
+    {
+        $id = $args['id'];
+        $persona = Persona::getPersonaById($id);
+
+        if (!$persona){
+            $rv = new stdclass();
+            $rv->message = "Recurso no encontrado";
+
+            $ret_response = $response->withJson($rv, 404);
+        }
+        else{
+            $ret_response = $response->withJson($persona, 200);
+        }
+        
+        return $ret_response;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public function getAll($request, $response, $args)
+    {
+        $personas = Persona::getAllPersonas();
+
+        $newResponse = $response->withJson($personas, 200);
+        return $newResponse;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    public function insert($request, $response, $args)
+    {        
+        $newPersonaData = $request->getParsedBody();
+        /*  Cada solicitud HTTP tiene un cuerpo. Si está creando una aplicación Slim
+        que consume datos JSON o XML, puede utilizar el método getParsedBody()
+        del objeto Request de PSR 7 para analizar el cuerpo de solicitud HTTP
+        en un formato PHP nativo. Slim puede analizar JSON, XML y datos codificados
+        por URL fuera de la caja.   */        
+
+        $passCrypt = crypt($newPersonaData["password"], "1af324D");
+        /* http://php.net/manual/es/function.crypt.php */
+
+        $newPersona = new Persona();
+        $newPersona->nombre = $newPersonaData["nombre"];
+        $newPersona->mail = $newPersonaData["mail"];
+        $newPersona->password = $passCrypt;
+        //[OPCIONAL] - $newPersona->password = crypt($newPersonaData["password"], "1af324D");
+        $newPersona->foto = $newPersonaData["foto"];
+        $newPersona->sexo = $newPersonaData["sexo"];
+
+        $personaid = $newPersona->insertPersona();
+
+        $rv = new stdclass();
+        $rv->message = "Persona ingresada";
+        
+        return $response->withJson($rv, 200);
+
+    }
+
+
+
+
+
+
+
+
+    public function update($request, $response, $args)
+    {
+        $newData = $request->getParsedBody();
+        $personaToUpdate = new Persona();
+        $personaToUpdate->id = $newData['id'];
+        $personaToUpdate->nombre = $newData['nombre'];
+        $personaToUpdate->mail = $newData['mail'];
+        $personaToUpdate->password = crypt($newData['password'], "1af324D");
+        $personaToUpdate->foto = $newData['foto'];
+        $personaToUpdate->sexo = $newData['sexo'];
+        $rv = new stdclass();
+
+        if ($personaToUpdate->updatePersona())
+        {
+            $rv->message = "El persona ha sido actualizado con exitosamente.";
+            $newResponse = $response->withJson($rv, 200);
+        }
+        else
+        {
+            $rv->message = "Hubo un error y no se ha podido actualizar. Comuniquese con el administrador de su sistema.";
+            $newResponse = $response->withJson($rv, 404);
+        }
+
+        return $newResponse;
+    }
+
+
+
+
+
+
+
+
+    
+    public function delete($request, $response, $args)
+    {
+        $personaToDelete = $request->getParsedBody();
+        $persona = new Persona();
+        $rv = new stdclass();
+
+        $id = $personaToDelete['id'];
+        $persona->id = $id;        
+
+        if ($persona->deletePersona() > 0)
+        {
+            $rv->message = "Persona eliminado exitosamente.";
+            $response = $response->withJson($rv, 200);
+        }
+        else
+        {
+            $rv->message = "Persona no encontrado.";
+            $response = $response->withJson($rv, 404);
+        }
+
+
+        return $response;
+    }
+
+
+
+
+
+
+
+
+    //SignIn
+    public function validatePersona($request, $response, $args)
+    {
+        try {
+            $rv = new stdclass();
+
+            $personaData = $request->getParsedBody();
+
+            $password = crypt($personaData['password'], "1af324D");
+            //$password = $personaData['password'];
+            $email = $personaData['mail'];
+
+            $persona = Persona::getPersonaDataByEmailAndPassword($email, $password);
+
+            
+            if ($persona != false)
+            {
+                $jwt = AuthJWT::getToken($persona);
+                $rv->jwt = $jwt;
+                $rv->message = 'Persona encontrado';
+                $response = $response->withJson($rv, 200);
+            }
+            else
+            {
+                
+                $rv->message = "El persona no ha sido encontrado";
+                $response = $response->withJson($rv, 404);
+            }
+
+
+            return $response;
+
+        } catch (Exception $ex) {
+            $rv->message = "Error desconocido. Comuniquese con el administrador de su sistema.";
+            $response = $response->withJson($rv, 404);
+            return $response;
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+    //SignUp
+    function registerPersona($request, $response, $args)
+    {
+        $rv = new stdclass();
+
+        $personaData = $request->getParsedBody();
+        $password = $personaData['password'];
+        $email = $personaData['mail'];
+
+        if (Persona::personaAlreadyExist($email)) {
+            $rv->message = "El persona ingresado ya existe";
+            $response = $response->withJson($rv, 404);
+        }
+        else {
+            $response = $this->insert($request, $response, $args);
+
+            $persona = new stdclass();
+            $persona->password = $password;
+            $persona->email = $email;
+            $jwt = AuthJWT::getToken($persona);
+            $rv->message = "Persona registrado exitosamente";
+            $rv->jwt = $jwt;
+            $response = $response->withJson($rv, 200);
+        }
+        return $response;
+    }
+
+
+
+
+
+
+/*
+
+
+    public static function subir($request, $response, $args)
+    {
+        $queHay = var_dump($_FILES);
+        $rv->archivo = var_dump($_FILES);
+
+        return $rv;
+    }
+*/
+ 
+
+}
---
+++ apirest/index.php
@@ -0,0 +1,66 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../composer/vendor/autoload.php';
+require '../composer/vendor/paragonie/random_compat/psalm-autoload.php';
+
+      //PHP 7.1.9
+      require_once dirname(__FILE__).'/classes/AccesoDatos.php';
+      require_once dirname(__FILE__).'/classes/AuthJWT.php';
+      require_once dirname(__FILE__).'/classes/MWCORS.php';
+      require_once dirname(__FILE__).'/classes/MWAuth.php';
+      require_once dirname(__FILE__).'/classes/personaApi.php';
+      
+      
+      /*    //PHP 5.3.0
+      require_once '/classes/AccesoDatos.php';
+      require_once '/classes/AuthJWT.php';
+      require_once '/classes/MWCORS.php';
+      require_once '/classes/MWAuth.php';
+      require_once '/classes/personaApi.php';
+      */
+
+      
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$app = new \Slim\App(["settings" => $config]);
+
+
+$app->group('/personas', function () {
+ 
+  $this->get('/', \personaApi::class . ':getAll');
+ 
+  $this->get('/{id}', \personaApi::class . ':getById');
+
+  $this->post('/insert', \personaApi::class . ':insert');
+
+  $this->post('/delete', \personaApi::class . ':delete');
+
+  $this->post('/update', \personaApi::class . ':update');
+
+  $this->post('/subirFoto', \personaApi::class . ':Subir');
+     
+})->add(\MWCORS::class . ':enableCORS');
+
+/*
+
+$app->group('/archivos', function () { 
+
+   $this->post('/subir', \personaApi::class . ':subir');     
+   
+ })->add(\MWCORS::class . ':enableCORS');
+
+*/
+
+$app->group('/login', function () {
+  
+   $this->post('/signin', \personaApi::class . ':validatePersona');
+  
+   $this->post('/signup', \personaApi::class . ':registerPersona');
+ 
+ })->add(\MWCORS::class . ':enableCORS');
+
+
+$app->run();
